4ef9d55c9f6705445543f80521c79c5db6c5db08
silas_270 <kammersi.to@gmail.com>
2026-04-21 12:28:41 +0200

    fix: coordinates for analysis 2 sheet

--- src/pdfEngine/engines/analysisEngine.js
+++ src/pdfEngine/engines/analysisEngine.js
@@ -20,8 +20,8 @@ export async function generateAnalysisPDF(mainPdfBytes, coverTemplateBytes, data
   // Text schreiben (Koordinaten aus deinem Python-Skript übernommen)
   // Hinweis: pdf-lib nutzt standardmäßig Standard-Fonts (Helvetica, Times Roman, etc.)
   coverPage.drawText(String(data.sheetNum), {
-    x: 167,
-    y: 586, 
+    x: 185,
+    y: 586,
     size: 14,
     color: rgb(0, 0, 0.5), // Navy (0, 0, 128) -> in pdf-lib 0 bis 1
   });
@@ -29,13 +29,13 @@ export async function generateAnalysisPDF(mainPdfBytes, coverTemplateBytes, data
   data.students.forEach((student, i) => {
     const yOffset = 449 - 29.5 * i;
     coverPage.drawText(`${student.firstName} ${student.lastName}`, {
-      x: 140,
+      x: 153,
       y: yOffset,
       size: 14,
       color: rgb(0, 0, 0.5),
     });
     coverPage.drawText(student.matNum, {
-      x: 380,
+      x: 340,
       y: yOffset,
       size: 14,
       color: rgb(0, 0, 0.5),
@@ -50,10 +50,10 @@ export async function generateAnalysisPDF(mainPdfBytes, coverTemplateBytes, data
 
   for (const page of copiedPages) {
     const { width: currentWidth } = page.getSize();
-    
+
     // Skalierungsfaktor berechnen (wie in deinem Python-Code)
     const scaleFactor = targetWidth / currentWidth;
-    
+
     if (scaleFactor !== 1) {
       page.scale(scaleFactor, scaleFactor);
     }
